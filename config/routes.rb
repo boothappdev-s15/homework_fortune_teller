a01c6435f1f2981256370062b9f2c2a610158ad9
Elias Stambolis <estambol@chicagobooth.edu>
2015-05-01 12:41:19 -0500

    part 2 complete (variable routes)

--- config/routes.rb
+++ config/routes.rb
@@ -16,16 +16,16 @@ Rails.application.routes.draw do
   get("/zodiacs/leo",         { :controller => "zodiacs",   :action => "lion" })
   get("/zodiacs/virgo",       { :controller => "zodiacs",   :action => "maiden" })
   get("/zodiacs/libra",       { :controller => "zodiacs",   :action => "scales" })
-  get("/zodiacs/scorpio",     { :controller => "zodiacs",  :action => "scorpion" })
-  get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  get("/zodiacs/scorpio",     { :controller => "zodiacs",   :action => "scorpion" })
+  get("/zodiacs/sagittarius", { :controller => "zodiacs",   :action => "archer" })
+  get("/zodiacs/capricorn",   { :controller => "zodiacs",   :action => "goat" })
+  get("/zodiacs/aquarius",    { :controller => "zodiacs",   :action => "waterbearer" })
+  get("/zodiacs/pisces",      { :controller => "zodiacs",   :action => "fish" })
 
 
   # PART 2: VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
 end
